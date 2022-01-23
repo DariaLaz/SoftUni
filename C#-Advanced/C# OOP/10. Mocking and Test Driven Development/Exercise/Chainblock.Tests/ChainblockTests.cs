50932275b34f442372767bd85925ec3ed98158b0
DariaLaz <daria.lazarova03@gmail.com>
2022-01-23 22:03:40 +0200

    upload Mocking and Test Driven Development

---
+++ C#-Advanced/C# OOP/10. Mocking and Test Driven Development/Exercise/Chainblock.Tests/ChainblockTests.cs
@@ -0,0 +1,42 @@
+﻿using Chainblock.Contracts;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chainblock.Tests
+{
+    [TestFixture()]
+    public class ChainblockTests
+    {
+        [Test]
+        public void AddMethodShouldWorkProperly()
+        {
+            IChainblock cb = new Chainblock();
+            ITransaction tr = new Transaction();
+            cb.Add(tr);
+            var exist = cb.Contains(tr);
+            Assert.True(exist);
+        }
+        [Test]
+        public void ContainsMethodShouldWorkWithTransaction()
+        {
+            IChainblock cb = new Chainblock();
+            ITransaction tr = new Transaction();
+            cb.Add(tr);
+            var exist = cb.Contains(tr);
+            Assert.True(exist);
+        }
+        [Test]
+        public void ContainsMethodShouldWorkWithGivenId()
+        {
+            IChainblock cb = new Chainblock();
+            ITransaction tr = new Transaction();
+            tr.Id = 1;
+            cb.Add(tr);
+            var exist = cb.Contains(1);
+            Assert.True(exist);
+        }
+        
+    }
+}
