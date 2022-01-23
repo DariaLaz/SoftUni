62e00274c2bd94f2433222ac9ea3fb366cfc854e
DariaLaz <daria.lazarova03@gmail.com>
2022-01-23 21:40:00 +0200

    upload Regular exam

---
+++ C#-Advanced/C# Advanced/09. Regular exam C# Advanced Exam - 23 October 2021/Regular exam/01. Food Finder/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Food_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vowels = new Queue<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray());
+            var consonants = new Stack<string>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray());
+
+            var pearDictionary = new Dictionary<string, int>();
+            pearDictionary.Add("p", 0);
+            pearDictionary.Add("e", 0);
+            pearDictionary.Add("a", 0);
+            pearDictionary.Add("r", 0);
+
+            var flourDictionary = new Dictionary<string, int>();
+            flourDictionary.Add("f", 0);
+            flourDictionary.Add("l", 0);
+            flourDictionary.Add("o", 0);
+            flourDictionary.Add("u", 0);
+            flourDictionary.Add("r", 0);
+
+            var porkDictionary = new Dictionary<string, int>();
+            porkDictionary.Add("p", 0);
+            porkDictionary.Add("o", 0);
+            porkDictionary.Add("r", 0);
+            porkDictionary.Add("k", 0);
+
+            var oliveDictionary = new Dictionary<string, int>();
+            oliveDictionary.Add("o", 0);
+            oliveDictionary.Add("l", 0);
+            oliveDictionary.Add("i", 0);
+            oliveDictionary.Add("v", 0);
+            oliveDictionary.Add("e", 0);
+
+            while (consonants.Count > 0)
+            {
+                var currentVowel = vowels.Dequeue();
+                var currentConsonant = consonants.Pop();
+
+                if (pearDictionary.ContainsKey(currentVowel))
+                {
+                    pearDictionary[currentVowel]++;
+                }
+                if (pearDictionary.ContainsKey(currentConsonant))
+                {
+                    pearDictionary[currentConsonant]++;
+                }
+
+                if (porkDictionary.ContainsKey(currentVowel))
+                {
+                    porkDictionary[currentVowel]++;
+                }
+                if (porkDictionary.ContainsKey(currentConsonant))
+                {
+                    porkDictionary[currentConsonant]++;
+                }
+
+                if (flourDictionary.ContainsKey(currentVowel))
+                {
+                    flourDictionary[currentVowel]++;
+                }
+                if (flourDictionary.ContainsKey(currentConsonant))
+                {
+                    flourDictionary[currentConsonant]++;
+                }
+
+                if (oliveDictionary.ContainsKey(currentVowel))
+                {
+                    oliveDictionary[currentVowel]++;
+                }
+                if (oliveDictionary.ContainsKey(currentConsonant))
+                {
+                    oliveDictionary[currentConsonant]++;
+                }
+
+                vowels.Enqueue(currentVowel);
+            }
+
+            var foundWords = new List<string>();
+
+            if (pearDictionary.Where(x => x.Value != 0).Count() == 4)
+            {
+                foundWords.Add("pear");
+            }
+            if (flourDictionary.Where(x => x.Value != 0).Count() == 5)
+            {
+                foundWords.Add("flour");
+            }
+            if (porkDictionary.Where(x => x.Value != 0).Count() == 4)
+            {
+                foundWords.Add("pork");
+            }
+            if (oliveDictionary.Where(x => x.Value != 0).Count() == 5)
+            {
+                foundWords.Add("olive");
+            }
+
+            Console.WriteLine($"Words found: {foundWords.Count}");
+            foreach (var word in foundWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ C#-Advanced/C# Advanced/09. Regular exam C# Advanced Exam - 23 October 2021/Regular exam/02. Pawn Wars/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace _02._Pawn_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = new char[8, 8];
+
+            var bRow = 0;
+            var bCol = 0;
+            var wRow = 0;
+            var wCol = 0;
+
+            var blacksTurn = false;
+            var whitesTurn = true;
+
+
+            for (int row = 0; row < 8; row++)
+            {
+                var line = Console.ReadLine();
+                for (int col = 0; col < 8; col++)
+                {
+                    board[row, col] = line[col];
+                    if (line[col] == 'b')
+                    {
+                        bRow = row;
+                        bCol = col;
+                    }
+                    else if (line[col] == 'w')
+                    {
+                        wRow = row;
+                        wCol = col;
+                    }
+                }
+            }
+            while (true)
+            {
+                if (bRow + 1 == wRow && (bCol + 1 == wCol || bCol - 1 == wCol))
+                {
+                    if (whitesTurn)
+                    {
+                        Console.WriteLine($"Game over! white capture on {ToCoordinates(bRow, bCol)}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Game over! black capture on {ToCoordinates(wRow, wCol)}.");
+                    }
+                    break;
+                }
+
+                if (whitesTurn)
+                {
+                    wRow--;
+                }
+                else
+                {
+                    bRow++;
+                }
+
+                if (wRow == 0)
+                {
+                    
+                    Console.WriteLine($"Game over! White pawn is promoted to a queen at {ToCoordinates(wRow, wCol)}");
+                    break;
+                }
+                if (bRow == 7)
+                {
+                    Console.WriteLine($"Game over! Black pawn is promoted to a queen at {ToCoordinates(bRow, bCol)}");
+                    break;
+                }
+
+                whitesTurn = !whitesTurn;
+                blacksTurn = !blacksTurn;
+            }
+        }
+
+        private static string ToCoordinates(int bRow, int bCol)
+        {
+            var col = "";
+            switch (bCol)
+            {
+                case 0: col = "a"; break;
+                case 1: col = "b"; break;
+                case 2: col = "c"; break;
+                case 3: col = "d"; break;
+                case 4: col = "e"; break;
+                case 5: col = "f"; break;
+                case 6: col = "g"; break;
+                case 7: col = "h"; break;
+            }
+            var row = "";
+            switch (bRow)
+            {
+                case 0: row = "8"; break;
+                case 1: row = "7"; break;
+                case 2: row = "6"; break;
+                case 3: row = "5"; break;
+                case 4: row = "4"; break;
+                case 5: row = "3"; break;
+                case 6: row = "2"; break;
+                case 7: row = "1"; break;
+            }
+            return (col + row);
+        }
+    }
+}
