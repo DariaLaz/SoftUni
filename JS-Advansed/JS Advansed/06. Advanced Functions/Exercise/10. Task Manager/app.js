34fe4e14838175ad52090d7cd6403ad825b877b5
DariaLaz <daria.lazarova03@gmail.com>
2022-02-05 21:42:07 +0200

    update task manager

--- JS-Advansed/JS Advansed/06. Advanced Functions/Exercise/10. Task Manager/app.js
+++ JS-Advansed/JS Advansed/06. Advanced Functions/Exercise/10. Task Manager/app.js
@@ -1,74 +1,73 @@
 function solve() {
-    let [addTaskSectionEl, openSectionElement, inProgressSectionElement, completeSectionElement] = document.querySelectorAll('section');
+    let [taskEL, dueDateEl] = document.querySelectorAll('form input')
+    let descriptionEl = document.querySelector('textarea')
+    let addBtn = document.querySelector('#add')
 
-    let addFunc = (e) => {
+    let [addTaskSection, openSection, inProgressSection, completeSection] = document.querySelectorAll('section div:nth-of-type(2)')
+    
+    let add = (e) => {
         e.preventDefault();
-        let [task, dueDate] = addTaskSectionEl.querySelectorAll('input');
-        let description = addTaskSectionEl.querySelector('textarea')
-        if(task.value && description.value && dueDate.value){
+        if(taskEL.value && dueDateEl.value && descriptionEl.value){
             let newArticle = document.createElement('article');
-            let newH3 = document.createElement('h3');
-            newH3.textContent = task.value
-            let newPDescr = document.createElement('p');
-            newPDescr.textContent = `Description: ${description.value}`
-            let newPDuedate = document.createElement('p');
-            newPDuedate.textContent = `Due Date: ${dueDate.value}`
-            let newDiv = document.createElement('div');
-            newDiv.classList.add('flex');
+    
+            let headingEl = document.createElement('h3');
+            headingEl.textContent = taskEL.value;
+            newArticle.appendChild(headingEl)
+    
+            let newDesrEl = document.createElement('p');
+            newDesrEl.textContent = `Description: ${descriptionEl.value}`;
+            newArticle.appendChild(newDesrEl)
+    
+            let newDueDateEl = document.createElement('p');
+            newDueDateEl.textContent = `Due Date: ${dueDateEl.value}`;
+            newArticle.appendChild(newDueDateEl)
+            
+            let btnsDiv = document.createElement('div');
+            btnsDiv.classList.add('flex')
 
             let startBtn = document.createElement('button');
-            startBtn.textContent = 'Start'
             startBtn.classList.add('green')
-            newDiv.appendChild(startBtn)
+            startBtn.textContent = 'Start'
             startBtn.addEventListener('click', start)
+            btnsDiv.appendChild(startBtn);
 
             let deleteBtn = document.createElement('button');
-            deleteBtn.textContent = 'Delete'
             deleteBtn.classList.add('red')
-            newDiv.appendChild(deleteBtn)
-            deleteBtn.addEventListener('click', deleteFunc)
+            deleteBtn.textContent = 'Delete'
+            deleteBtn.addEventListener('click', del)
+            btnsDiv.appendChild(deleteBtn);
 
-            newArticle.appendChild(newH3)
-            newArticle.appendChild(newPDescr)
-            newArticle.appendChild(newPDuedate)
-            newArticle.appendChild(newDiv)
+            newArticle.appendChild(btnsDiv)
 
-            openSectionElement.querySelectorAll('div')[1].appendChild(newArticle)
-            task.value = ''
-            description.value = ''
-            dueDate.value = ''
+            openSection.appendChild(newArticle)
 
+            taskEL.value = ''
+            dueDateEl.value = ''
+            descriptionEl.value = ''
         }
     }
     let start = (e) => {
-        let articleToMove = e.currentTarget.parentElement.parentElement;
-        articleToMove.querySelector('div').remove()
-        let btnDivEl = document.createElement('div');
+        let articleToMove = e.currentTarget.parentElement.parentElement
 
-        let deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete'
-        deleteBtn.classList.add('red')
-        deleteBtn.addEventListener('click', deleteFunc)
-        btnDivEl.appendChild(deleteBtn)
+        let finishBtn = document.createElement('button');
+        finishBtn.classList.add('orange')
+        finishBtn.textContent = 'Finish'
+        finishBtn.addEventListener('click', finish)
+        e.currentTarget.parentElement.appendChild(finishBtn);
 
-        let finishBtb = document.createElement('button');
-        finishBtb.textContent = 'Finish'
-        finishBtb.classList.add('orange')
-        finishBtb.addEventListener('click', finish)
-        btnDivEl.appendChild(finishBtb)
+        e.currentTarget.remove()
 
-        articleToMove.appendChild(btnDivEl)
-
-        inProgressSectionElement.querySelectorAll('div')[1].appendChild(articleToMove)
+        inProgressSection.appendChild(articleToMove)
     }
-    let deleteFunc = (e) => {
+    let del = (e) => {
         e.currentTarget.parentElement.parentElement.remove()
     }
     let finish = (e) => {
-        let articleToComplete = e.currentTarget.parentElement.parentElement;
-        articleToComplete.querySelector('div').remove()
-        
-        completeSectionElement.querySelectorAll('div')[1].appendChild(articleToComplete)
+        let articleToMove = e.currentTarget.parentElement.parentElement
+        e.currentTarget.parentElement.remove();
+        completeSection.appendChild(articleToMove)
     }
-    document.querySelector('#add').addEventListener('click', addFunc)
+
+
+    addBtn.addEventListener('click', add)
 }
