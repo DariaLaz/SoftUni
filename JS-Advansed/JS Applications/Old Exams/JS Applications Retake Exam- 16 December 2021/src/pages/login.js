50dc7def2ae4b112a0dba0384b4293322452c57b
DariaLaz <daria.lazarova03@gmail.com>
2022-04-02 08:18:36 +0300

    exam

--- JS-Advansed/JS Applications/Old Exams/JS Applications Retake Exam- 16 December 2021/src/pages/login.js
+++ JS-Advansed/JS Applications/Old Exams/JS Applications Retake Exam- 16 December 2021/src/pages/login.js
@@ -32,7 +32,7 @@ export async function loginPage(ctx){
         const email= formData.get('email')
         const password= formData.get('password')
         if(email == '' || password == ''){
-            return alert('Pleasse fill both fields')
+            return alert('Please fill both fields')
         }
 
         await loginUser(email, password);
