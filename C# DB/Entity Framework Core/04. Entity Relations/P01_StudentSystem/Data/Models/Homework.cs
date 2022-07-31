087dc2ed6b8494609c53ccf9dc2c8668b4cb5264
DariaLaz <daria.lazarova03@gmail.com>
2022-08-01 02:13:40 +0300

    P01_StudentSystem

---
+++ C# DB/Entity Framework Core/04. Entity Relations/P01_StudentSystem/Data/Models/Homework.cs
@@ -0,0 +1,37 @@
+﻿using P01_StudentSystem.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        [Key]
+        public int HomeworkId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public ContentTypeEnum ContentType { get; set; }
+
+        [Required]
+        public DateTime SubmissionTime { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+        [ForeignKey(nameof(StudentId))]
+        public Student Student { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public Course Course { get; set; }
+
+
+    }
+}
---
+++ C# DB/Entity Framework Core/04. Entity Relations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        public virtual DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public virtual DbSet<Course> Courses { get; set; }
+
+        public virtual DbSet<Resource> Resources { get; set; }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        public virtual DbSet<StudentCourse> StudentCourses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .Property(s => s.PhoneNumber)
+                .IsUnicode(false)
+                .IsRequired(false);
+            modelBuilder.Entity<Course>()
+                .Property(s => s.Name)
+                .IsUnicode(true);
+            modelBuilder.Entity<Course>()
+                .Property(s => s.Description)
+                .IsUnicode(true);
+            modelBuilder.Entity<Resource>()
+               .Property(s => s.Name)
+               .IsUnicode(true);
+            modelBuilder.Entity<Resource>()
+               .Property(s => s.Url)
+               .IsUnicode(false);
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.CourseId, sc.StudentId });
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=StudentSystem;User Id=sa;Password=SoftUn!2021");
+            }
+        }
+    }
+}
